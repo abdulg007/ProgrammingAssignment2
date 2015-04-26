9ea546fae786a93600327fb78ea5367445334991
Abdul Ghaffar <abdulg007@yahoo.com>
2015-04-26 17:42:23 -0400

    Programming Assignment #2 - Calculating Inverse Matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,51 @@
-## Put comments here that give an overall description of what your
-## functions do
+## R-Programming - Assignment #2
+## Abdul Ghaffar
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+##  Create Special type of matrix that caches inverseMatrix for later retrieval from matrix passed into it
+makeCacheMatrix <- function (x = matrix()) {
+  ## Create a special type of matrix with functions to set and get inverse of a given Matrix
+  invMatrix <- NULL
+  
+  set <- function(y) {
+    x <<- y
+    invMatrix <<- NULL
+  }
+  
+  get <- function() {
+    x
+  }
+  
+  setInv <- function(inv) {
+    # Setting InverseMatrix here
+    invMatrix <<- inv
+  }
+  
+  getInv <- function() {
+    # Getting InverseMatrix ...
+    invMatrix
+  }
+  
+  list(set=set, get=get, setInv=setInv, getInv=getInv) 
 }
 
 
-## Write a short comment describing this function
-
+# This function first checks for a cached inverseMatrix first; otherwise calcualates/sets/returns a new 
+# inverseMatrix which could be retrieved later.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  invMatrix <- x$getInv()
+  
+  if (!is.null(invMatrix)) {
+    message(">>> Cached Data found - Returning Cached Data")
+    return (invMatrix)
+  }
+  
+  ## If no inverseMatrix returned, calcuate a new one, set it up and return the results.
+  message (">>> No Cached Data - Calculating new InverseMatrix for this time")
+  
+  data <- x$get()
+  x$set(data)
+  
+  invMatrix <- solve(data)
+  x$setInv(invMatrix)
+  invMatrix
 }
